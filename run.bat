491f1398dec66c936fd4713d3457253726f73dc5
sooraj <rajsooraj30@gmail.com>
2020-12-13 10:21:55 +0530

    third commit

--- run.bat
+++ run.bat
@@ -2,7 +2,7 @@
 rem "rem denotes that this line is a comment"
 
 rem ******************running on chrome***************************
-pytest -s -v -m "sanity or regression" --html=Reports\report.html testCases
+pytest -s -v -m "sanity or regression" testCases
 rem pytest -s -v -m "sanity and regression" --html=Reports\report.html testCases
 rem pytest -s -v -m "sanity" --html=Reports\report.html testCases
 rem pytest -s -v -m "regression" --html=Reports\report.html testCases
